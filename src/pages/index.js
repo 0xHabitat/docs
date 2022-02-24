b6cbe5818bb3d9cd6c56f6e30a66e99933fe6f9a
tjvsx <vanslotj@gmail.com>
2022-02-24 02:07:46 -0500

    style detailing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Loadable from '@docusaurus/react-loadable'
 import CoverSection from './sections/CoverSection'
+import loading from './loading.module.css';
 
 const LandingPage = Loadable({
   loader: () => import('./LandingPage'),
-  loading: () => <div>Loading...</div>
+  loading: () => 
+  <div className={loading.loader}>
+    <h1 className={loading.sliding}>🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏 🌎 🌍 🌏
+    </h1>
+  </div>
 });
 
 export default function Home() {
